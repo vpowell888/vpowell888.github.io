ae0ae7bcb6f17def686ea410b71b5677534519eb
vpowell888 <vincent.powell@oblongix.com>
2025-06-25 21:09:25 +0100

    Updates

--- server.js
+++ server.js
@@ -9,41 +9,6 @@ app.use(express.static(__dirname));
 
 app.use(express.json()); 
 app.use(bodyParser.urlencoded({ extended: false }));
-// Email route
-app.post('/contact', async (req, res) => {
-  const { name, email, message } = req.body;
-
-  // Configure your SMTP transporter
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.office365.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    },
-    tls: {
-      ciphers: 'SSLv3'
-    }, logger: true,
-  debug: true
-  });
-
-  // Email content
-  const mailOptions = {
-    from: email,
-    to: process.env.EMAIL_RECEIVER,
-    subject: `Contact Form from ${name}`,
-    text: `You received a message:\n\nFrom: ${name} <${email}>\n\nMessage:\n${message}`
-  };
-
-  try {
-    await transporter.sendMail(mailOptions);
-    res.send('<h1>Message sent successfully!</h1><a href="/">Go back</a>');
-  } catch (error) {
-    console.error('Email error:', error);
-    res.send('<h1>Error sending message. Please try again later.</h1>');
-  }
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
